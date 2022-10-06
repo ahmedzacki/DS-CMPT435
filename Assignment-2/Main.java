e948561191cd3f412203e3f2ae93c02d4aa15aca
ahmedzacki <ahmedxandulle01@gmail.com>
2022-10-06 15:56:58 -0400

    Deleted the Sorting file since I didn't need it, therefore all my programs will be in my Main.java file

--- Assignment-2/Main.java
+++ Assignment-2/Main.java
@@ -1,48 +1,54 @@
-import java.io.File; // importing file utility package 
+import java.io.File; // importing file utility package to manage our file
 import java.util.Scanner; // Importing the Scanner class to read text files
-import java.util.ArrayList; // import the ArrayList class
+import java.util.ArrayList; // importing the ArrayList class to store elements
+import java.util.Collections; // For swaping elements
 
 public class Main {
 
+    public static ArrayList<String> selectionSort(ArrayList<String> str) {
+        if (str == null) {
+            return null;
+        }
+        int comparisonCount = 0;
+        int n = str.size();
+        for(int i=0; i<n-1; i++){
+            int curpos = i;
+            for(int j= i+1; j<n; j++){
+                countComparison++;
+                int result = str.get(j).compareToIgnoreCase(str.get(curpos));
+                if(result<0){
+                    curpos = j;
+                }
+            }
+            Collections.swap(str, i, curpos);   
+        }
+        System.out.println("Number of Comparisons: "+ comparisonCount);
+        return str;
+    }
 
     public static void main(String[] args) {
-
-
         // Creating an ArrayList object to store lines of strings
-        ArrayList<String> strArry = new ArrayList<String>();
+        ArrayList<String> strArry = new ArrayList<String>();       
       
         try { 
             // creating a new file object
             File f = new File("magicitems.txt");
-
-            // creating an object of Scanner
-            // associated with the file
+            // creating a Scanner Object
             Scanner myReader = new Scanner(f);
-
-            // reading each line and
-            // removing the spaces and making it all lowercase
+            // reading each line and removing the spaces and making it all lowercase
             while(myReader.hasNextLine()) {
                 String linee = myReader.nextLine();
-                strArry.add(linee.toLowerCase().replace(" ", ""));
+                strArry.add(linee.replaceAll("[^A-Za-z]", "").toLowerCase());
                 }
             // closing scanner
-            myReader.close();
-            
+            myReader.close();    
             // Now catching to see if any errors exist in processing this program file
           } catch (Exception e) {
             e.getStackTrace();
           }
 
-        // going to the array and get the strings that are Palindrome
-        for (int i = 0; i< strArry.size(); i++){
-            System.out.println(strArry.get(i));
-        }
         System.out.println("Size of the ArrayList: "+ strArry.size());
-
-        int[] arr = {3,4,2,1};
-
-        Sorting.selectionSort(arr);
-
+        System.out.println(selectionSort(strArry));
 
     
     }
