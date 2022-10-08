cfb081c1464a55831bd996773977688f765ffd22
ahmedzacki <ahmedxandulle01@gmail.com>
2022-10-08 03:55:16 -0400

    Merge Sort Implementation Using ArrayListr of Strings

--- Assignment-2/Main.java
+++ Assignment-2/Main.java
@@ -17,7 +17,7 @@ public class Main {
         }
     }
 //Selection Sort
-    public static ArrayList<String> selectionSort(ArrayList<String> str) {
+    public static ArrayList<String> selectionSort(ArrayList<String> str){
         if (str == null) {
             return null;
         }
@@ -63,10 +63,57 @@ public class Main {
         System.out.println("Number of Comparisons: "+ (swapComparison+nonSwapComparison));
         return str;
     }
-    
+//Merge Sort
+    public static ArrayList<String> mergeSort(ArrayList<String> str) {
+        shuffleArrList(str);
+        mergeSortCall(str, 0, str.size()-1);
+        return str;
+    }
+    public static void mergeSortCall(ArrayList<String> str, int leftEndIndex, int rightEndIndex) {
+
+        if(leftEndIndex<rightEndIndex){
+            int mid = (leftEndIndex+rightEndIndex)/2;
+            mergeSortCall(str, leftEndIndex, mid);
+            mergeSortCall(str, mid+1, rightEndIndex);
+            merge(str, leftEndIndex, mid, rightEndIndex);
+        }      
+    }
+
+    private static void merge(ArrayList<String> str, int leftEndIndex, int mid, int rightEndIndex) {
+        ArrayList<String> tempArray = new ArrayList<String>(str);
+        int i = leftEndIndex;
+        int j = mid+1;
+        int tempArrayIndex = leftEndIndex;
+
+        while((i<= mid) && (j<=rightEndIndex)){
+            if(str.get(i).compareTo(str.get(j))<0){
+                tempArray.set(tempArrayIndex, str.get(i));
+                i++;
+            } else {
+                tempArray.set(tempArrayIndex, str.get(j));
+                j++;
+            }
+            tempArrayIndex++;
+        }
+        while (i<= mid){
+            tempArray.set(tempArrayIndex, str.get(i));
+            i++;
+            tempArrayIndex++;
+        }
+        while (j<=rightEndIndex) {
+            tempArray.set(tempArrayIndex, str.get(j));
+            j++;
+            tempArrayIndex++;
+        }
+        for(int x=leftEndIndex; x <rightEndIndex+1; x++){
+            str.set(x, tempArray.get(x));
+        }
+    }
+// Quick Sort 
+
     public static void main(String[] args) {
         // Creating an ArrayList object to store lines of strings
-        ArrayList<String> strArry = new ArrayList<String>();    
+        ArrayList<String> strArry = new ArrayList<String>();
 
         try { 
             // creating a new file object
@@ -91,6 +138,8 @@ public class Main {
         System.out.println("<<Insertion Sort>>");
         System.out.println("Size of the ArrayList: "+ strArry.size());
         System.out.println(insertionSort(strArry));
-    
+        System.out.println("<<Merge Sort>>");
+        System.out.println("Size of the ArrayList: "+ strArry.size());
+        System.out.println(mergeSort(strArry));   
     }
 }
