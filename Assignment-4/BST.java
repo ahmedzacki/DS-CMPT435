53105d9501e0300fd4439b981fd56eea407f1c70
ahmedzacki <ahmedxandulle01@gmail.com>
2022-11-22 21:45:13 -0500

    Binary Search Part Complete

--- Assignment-4/BST.java
+++ Assignment-4/BST.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList; // importing the ArrayList class to store elements
+
 public class BST {
 
     // This Node class will be used to store elements
@@ -11,7 +13,6 @@ public class BST {
             this.left = null;
             this.right = null;
         }
-
     }
 
     // Initializing the root node of the BST
@@ -24,38 +25,48 @@ public class BST {
 
     public void insert(String string) {
 
+        // Creating an CharacterArrayList to store the path of each node
+        ArrayList<Character> pathArray = new ArrayList<Character>();
+
         Node newNode = new Node(string);
 
         if (this.root == null) {
             this.root = newNode;
         } else {
-            // Temporary node for storing the reference of the parent of the position of the new node
+
+            // Temporary node for storing the root of the tree
             Node temp = this.root;
+            // Keeping truck of the parent node of the position where the new node will be inserted
             Node refPositionOfNewParentNode = null;
+
+            // Find the position of the new Node 
             while (temp != null) {
                 refPositionOfNewParentNode = temp;
                 if (newNode.data.compareTo(temp.data) < 0) {
                     temp = temp.left;
+                    pathArray.add('L');
+
                 } else if (newNode.data.compareTo(temp.data) > 0) {
                     temp = temp.right;
+                    pathArray.add('R');
                 } else {
                     return;
                 }
             }
-            // Check if the new node is greater or less than the parent node and insert newNode 
+            // Printing out the path of the Node in a CharacterArrayList
+            System.out.println(pathArray);
 
+            // Check if the new node is greater or less than it's parent and insert newNode in it's correct positioin
             if (newNode.data.compareTo(refPositionOfNewParentNode.data) < 0) {
                 refPositionOfNewParentNode.left = newNode;
             } else {
                 refPositionOfNewParentNode.right = newNode;
             }
         }
-
     }
-    // Printing elements in In-order-traversal (left, root, right) using Recursiosn
 
+    // Printing elements in In-order-traversal (left, root, right) using Recursiosn
     public void inorder(Node root) {
-        
         if (root != null) {
             inorder(root.left);
             System.out.println(root.data);
@@ -63,4 +74,54 @@ public class BST {
         }
     }
 
+    // Storing the number of comparison of each look up in this arrayList 
+    ArrayList<Integer> totalComparisonCount = new ArrayList<Integer>();
+
+    // This function returns the average comparisons of each look up 
+    public Double avgSearchComparison(ArrayList<Integer> comparisonCounting) {
+
+        int sum = 0;
+        for (int i = 0; i < comparisonCounting.size(); i++) {
+            sum += comparisonCounting.get(i);
+        }
+        double avg = sum / comparisonCounting.size();
+
+        return avg;
+    }
+
+    // Searching elements in the BST and retrun their path in a characterArrayListx
+    public void search(String element) {
+
+        int comparisonsCount = 0;
+
+        // Creating an CharacterArrayList to store the path of each node
+        ArrayList<Character> pathArray2 = new ArrayList<Character>();
+
+        Node treeRoot = this.root;
+
+        while (treeRoot != null) {
+            if (element.compareTo(treeRoot.data) < 0) {
+                treeRoot = treeRoot.left;
+                pathArray2.add('L');
+                comparisonsCount++;
+            } else if (element.compareTo(treeRoot.data) > 0) {
+                treeRoot = treeRoot.right;
+                pathArray2.add('R');
+                comparisonsCount++;
+            } else {
+                //Printing the look up path
+                System.out.println("Look-up-Path: " + pathArray2);
+                // Printing the number of comparison for each look up 
+                System.out.println("Number of comparisons: " + comparisonsCount);
+                break;
+            }
+        }
+        // Check if the element is not in the tree 
+        if (treeRoot == null) {
+            System.out.println(element + " : is not in the tree");
+        }
+
+        //Adding the comparison count to the totalComparisonCount arrayList to calculate the average
+        totalComparisonCount.add(comparisonsCount);
+    }
 }
--- Assignment-4/Main.java
+++ Assignment-4/Main.java
@@ -5,44 +5,80 @@ import java.util.ArrayList; // importing the ArrayList class to store elements
 
 public class Main {
 
-
     public static void main(String[] args) {
         // Creating an ArrayList object to store lines of strings
-        ArrayList<String> strArry = new ArrayList<String>();
+        ArrayList<String> magicItems = new ArrayList<String>();
+
+        // Creating another ArrayList to store the magic items we are searcing 
+        ArrayList<String> magicItemsFind = new ArrayList<String>();
 
         try {
             // creating a new file object
             File f = new File("magicitems.txt");
+
+            // Creating another file for accessing the second magic items file
+            File f2 = new File("magicitems-find-in-bst.txt");
+
             // creating a Scanner Object
             Scanner myReader = new Scanner(f);
+
+            // Creating another Scanner object for the magicItemsFind file 
+            Scanner myReader2 = new Scanner(f2);
+
             // reading each line and removing the spaces and making it all lowercase
             while (myReader.hasNextLine()) {
                 String linee = myReader.nextLine();
-                strArry.add(linee.replaceAll("[^A-Za-z]", "").toLowerCase());
+                magicItems.add(linee.replaceAll("[^A-Za-z]", "").toLowerCase());
+            }
+
+            // Reading each line of the second magic items file and filtering it, then will store in the array List
+            while (myReader2.hasNextLine()) {
+                String line2 = myReader2.nextLine();
+                magicItemsFind.add(line2.replaceAll("[^A-Za-z]", "").toLowerCase());
             }
             // closing scanner
             myReader.close();
+            // Closing the second scanner
+            myReader2.close();
             // Now catching to see if any errors exist in processing this program file
         } catch (Exception e) {
             e.getStackTrace();
         }
 
-        // Converting the String ArrayList to String Array of the same length
-        String[] arr = new String[strArry.size()];
-        for (int i = 0; i < strArry.size(); i++) {
-            arr[i] = strArry.get(i);
+        // Converting the String ArrayList to String Array
+        String[] magicItemsArray = new String[magicItems.size()];
+        for (int i = 0; i < magicItems.size(); i++) {
+            magicItemsArray[i] = magicItems.get(i);
+        }
+
+        // Converting the magicItemsFind ArrayList to String Array
+        String[] magicItemsFindArray = new String[magicItemsFind.size()];
+        for (int i = 0; i < magicItemsFind.size(); i++) {
+            magicItemsFindArray[i] = magicItemsFind.get(i);
         }
 
         // Creating an object of BST class 
         BST binarySearchTree = new BST();
 
+        System.out.println("Populating the BST with elements and printing their path");
         // Populating the BST with the magic items 
-        for (String eachstring : arr) {
+        for (String eachstring : magicItemsArray) {
             binarySearchTree.insert(eachstring);
         }
 
+        System.out.println("Printing the elements in the tree in In-Order-Traversal");
+
         // Printing magic items in In-Order-Traversals
         binarySearchTree.inorder(binarySearchTree.root);
+
+        System.out.println("Printing the path of each of the searched element in the BST");
+        // Searching all the strings in the magicItemsFindArray one by one
+        for (String eachString : magicItemsFindArray) {
+            binarySearchTree.search(eachString);
+        }
+
+        // Calculating the Average comparison count for the searched elements 
+        System.out.println("Average Comparison Count: " + binarySearchTree.avgSearchComparison(binarySearchTree.totalComparisonCount));
 
     }
 }
