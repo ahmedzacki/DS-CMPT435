478db8495375510159fc75f196a797c61fa27625
ahmedzacki <ahmedxandulle01@gmail.com>
2022-12-04 01:55:56 -0500

    Edited the latext doc

--- Assignment-4/LinkedObjects.java
+++ Assignment-4/LinkedObjects.java
@@ -1,16 +1,11 @@
 import java.util.ArrayList;
 
 public class LinkedObjects {
-    public static void main(String[] args) {
-        
-    }
-
-    // Object Attributes => Each Vertex is represented as an object
+    // Object Attributes => Each Vertex is represented as an object with attributes as class variables
     int id;
     Boolean processed;
     ArrayList<LinkedObjects> neighbors;
     
-
     // Initialize the class/Vertex object
     LinkedObjects(int id) {
         this.id = id;
